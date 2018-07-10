07076283465d84ee9c9f72dc354199179f4dbaa2
Alex Vidrean <alex.vidrean@gmail.com>
2018-07-10 22:54:43 +0300

    Possibility to spread Love

--- app/src/main/java/ro/alexv/mclovin/MainActivity.kt
+++ app/src/main/java/ro/alexv/mclovin/MainActivity.kt
@@ -1,10 +1,19 @@
 package ro.alexv.mclovin
 
+import android.app.AlertDialog
+import android.graphics.Point
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.google.ar.core.Anchor
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +26,57 @@ class MainActivity : AppCompatActivity() {
 
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
 
+        fab.setOnClickListener {
+            addObject(Uri.parse("Heart.sfb"))
+        }
     }
+
+    private fun addObject(uri: Uri) {
+        val frame = fragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(fragment, hit.createAnchor(), uri)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun getScreenCenter(): android.graphics.Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
+    }
+
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+                .setSource(fragment.context, model)
+                .build()
+                .thenAccept {
+                    addNodeToScene(fragment, anchor, it)
+                }
+                .exceptionally {
+                    val builder = AlertDialog.Builder(this)
+                    builder.setMessage(it.message)
+                            .setTitle("error!")
+                    val dialog = builder.create()
+                    dialog.show()
+                    return@exceptionally null
+                }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
